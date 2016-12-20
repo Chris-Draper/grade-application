97a81024a5d3f7b8a1f4d57955427b271fc3c76e
Chris Draper <chrisd9809@gmail.com>
2016-12-20 11:19:16 -0500

    S2_Craft-tabs Fix the spacing that I fucked up before its too late

--- js/GradeDisplay.jsx
+++ js/GradeDisplay.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class GradesDisplay extends React.Component {
-	render() {
-		return (
-			<h1>This rendered</h1>
-		);
-	}
+    render() {
+        return (
+            <h1>This rendered</h1>
+        );
+    }
 }
 
 ReactDOM.render(
-	<GradesDisplay />,
-	document.getElementById('content')
+    <GradesDisplay />,
+    document.getElementById('content')
 );
